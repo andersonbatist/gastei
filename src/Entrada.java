c2d73971d3b8b95f6ee309c9dc4fb0b7f8cb5087
andersonbatist <andersonbatista.home@gmail.com>
2025-07-05 20:16:42 -0300

    Adicionei um println que irei excluir depois

--- src/Entrada.java
+++ src/Entrada.java
@@ -3,6 +3,7 @@ public class Entrada {
     public static void main(String[] args) throws Exception {
         
         Metodos.mostrarTelaAbertura();
+        System.out.println("AAAAAAAAAAAAAAAAAAAA");
 
     }
 }
