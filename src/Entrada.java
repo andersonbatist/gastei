6b3008bd643aea01804da9a5a038fa36fb4d07ed
andersonbatist <andersonbatista.home@gmail.com>
2025-07-05 20:20:00 -0300

    Excluindo o println

--- src/Entrada.java
+++ src/Entrada.java
@@ -3,7 +3,6 @@ public class Entrada {
     public static void main(String[] args) throws Exception {
         
         Metodos.mostrarTelaAbertura();
-        System.out.println("AAAAAAAAAAAAAAAAAAAA");
 
     }
 }
